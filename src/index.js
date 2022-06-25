d4c28654c9f11281399ed3b2416fa9b010163564
yessetz <yessetkhan@gmail.com>
2022-06-25 17:40:56 +0500

    feat: add function in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    
+    if (typeof n != "number")
+        return 0;
+    
+    n = Math.abs(n);
+    let length = n.toString().length - 1;
+    let reverse = 0;
+
+    for ( ; length >= 0; length--) {
+        reverse += Math.abs(n) % 10 * (10 ** length);
+        n = Math.floor(n / 10);
+    }
+
+    return reverse;
 }
